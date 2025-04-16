a10bd017e2944b95e86e0ef12512a62a49358eea
MacOS m3 <uzbzarif@gmail.com>
2025-04-16 17:18:16 +0500

    get listo fo user,project and task

--- main.py
+++ main.py
@@ -17,13 +17,33 @@ configuration = Configuration(
 )
 
 # Get the list of users
+print("List of users:")
 with ApiClient(configuration) as api_client:
     # Get list of users
     data,response = api_client.users_api.list()
-    pprint(data)
-
+    # Get results
+    results = data.get("results")
+    # print names of users
+    for user in results:
+        print(user.get("username"))
+    
+print("\nList of groups:")
 # Get the list of projects
 with ApiClient(configuration) as api_client:
     # Get list of projects
     data,response = api_client.projects_api.list()
-    pprint(data)
+    # Get results
+    results = data.get("results")
+    # print names of projects
+    for project in results:
+        print(project.get("name"))
+print("\nList of tasks:")
+# Get the list of tasks
+with ApiClient(configuration) as api_client:
+    # Get list of tasks
+    data,response = api_client.tasks_api.list()
+    # Get results
+    results = data.get("results")
+    # print names of tasks
+    for task in results:
+        print(task.get("name"))
